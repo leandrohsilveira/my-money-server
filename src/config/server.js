268c3b1cebc8124da18b32fb5d5d408aaec43616
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2017-04-25 12:59:18 -0300

    CORS and Query INT Middlewares

--- src/config/server.js
+++ src/config/server.js
@@ -2,11 +2,15 @@ const port = 3003
 
 const bodyParser = require('body-parser')
 const express = require('express')
+const allowCors = require('./cors')
+const expressQueryInt = require('express-query-int')
 
 const server = express()
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
+server.use(allowCors)
+server.use(expressQueryInt())
 
 server.listen(port, function() {
     console.log(`The SERVER is running on port ${port}`)
